7c2ff13c575fe9f41ce6c92f6e887d0528136c8c
Mehdi <mehdi.lemaire.pro@gmail.com>
2024-11-29 11:06:37 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+document.getElementById
+
+let baliseZoneInput = document.getElementById("inputEcriture")
+console.log(baliseZoneInput);
+
+let baliseZonebtn = document.getElementById("btnValiderMot")
+console.log(baliseZonebtn);
+
+let baliseZoneProposition = document.querySelector(".zoneProposition")
+console.log(baliseZoneProposition);
+
+let baliseZoneScore = document.querySelector(".zoneScore span")
+console.log(baliseZoneScore);
+
+let baliseZoneRadio = document.querySelectorAll(".optionSource input")
+console.log(baliseZoneRadio);
+
+let baliseimg = document.getElementById("sss");
+
+baliseimg.classList.add("nouvelleclasse")
+baliseimg.classList.remove("photo")
+
